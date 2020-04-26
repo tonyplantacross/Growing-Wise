7711eaeac480a51ad71a8acc8694611e7f5d5ed5
mnazir <monazir1001@gmail.com>
2020-04-26 01:02:02 +0100

    Added layout components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './static/css/style.css';
-import Header from './Components/Header.js';
+import Header from './Components/Layout/Header';
+import Footer from './Components/Layout/Footer';
+import Main from './Components/Main.js';
+import Banner from './Components/Layout/Banner';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header container-fluid">
         <Header />
+        <Banner />
       </header>
+      <main class="container-fluid">
+        <Main />
+      </main>
+      <footer class="container-fluid">
+        <Footer />
+      </footer>
     </div>
   );
 }
